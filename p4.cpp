909196ce89c3770f787b1eba3440aca85a9de1c3
fanfan <57862422+Vegetable-Code@users.noreply.github.com>
2021-01-11 14:59:33 +0800

    p4.cpp
    /* 参考示例 
    1 a 10 20 30
    2 b 15 25 35
    3 c 26 36 46
    4 d 45 62 13 
    5 e 58 56 88
    6 f 92 54 23
    7 g 85 64 21
    8 h 82 32 54
    9 i 95 64 21
    10 j 100 98 43
    */

--- p4.cpp
+++ p4.cpp
@@ -1,31 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#define MAX_NUM 10
+#define ALL_cls 3
+
+// 延迟函数 
+void delay() {
+	Sleep(1000);
+	printf("."); 
+	Sleep(1000);
+	printf("."); 
+	Sleep(1000);
+	printf(".\n"); 
+	Sleep(1000);
+}
+
 // 学生信息 
 struct Student{
 	char stuNum[20];
 	char name[20];
-	float score[3];
+	float score[ALL_cls];
+	float average;
 }; 
 
-// 功能一 
-struct Student stu[10];
+struct Student stu[MAX_NUM];
 
-int main() {
-	int i, j, k;
-	float maxScore = 0, sum[10] = {0}; 
-	// 功能二 
-	for(i = 0; i < 2; i++) {
-		printf("请输入第%d个学生的信息：", i + 1);
-		scanf("%s %s", stu[i].stuNum, &stu[i].name);
-		for(j = 0; j < 3; j++) {
-			scanf("%f",&stu[i].score[j]);
-		}
+// 输入函数
+void input() {
+	FILE *fp;
+	int i; 
+	printf("**********学生信息录入**********\n\n"); 
+	if((fp = fopen("stud.dat", "wb")) == NULL) {
+		printf("cannot open file\n");
 	}
-	// 功能三 
-	k = 0;
+	for(i = 0; i < MAX_NUM; i++) {
+		printf("\t");
+		scanf("%s %s %f %f %f", stu[i].stuNum, &stu[i].name, &stu[i].score[0], &stu[i].score[1], &stu[i].score[2]);
+		fwrite(&stu[i], sizeof(struct Student), 1, fp);
+		fclose(fp);
+	}
+	printf("\n\t保存ing");
+	delay();
+	printf("\n************录入成功************\n"); 
+} 
+
+// 单门成绩最高
+void single() {
+	float maxScore = 0;
+	int i, j, k = 0;
 	maxScore = stu[0].score[0];
-	for(i = 0; i < 2; i++) {
-		for(j = 0; j < 3; j++) {
+	for(i = 0; i < MAX_NUM; i++) {
+		for(j = 0; j < ALL_cls; j++) {
 			if(stu[i].score[j] > maxScore) {
 				k = i;
 				maxScore = stu[i].score[j];
@@ -33,22 +58,58 @@ int main() {
 		}
 	}
 	printf("\n单门成绩最高为第%d名同学：%s %s %.2f\n", k + 1, stu[k].stuNum, stu[k].name, maxScore);
-	// 功能四
-	for(i = 0; i < 2; i++) {
-		for(j = 0; j < 3; j++) {
+} 
+
+// 平均分最高
+void aver() {
+	int i, j;
+	float sum[MAX_NUM] = {0}, average[MAX_NUM] = {0}, maxAver = 0;
+	for(i = 0; i < MAX_NUM; i++) {
+		for(j = 0; j < ALL_cls; j++) {
 	 		sum[i] += stu[i].score[j];
 	 	}
+	 	average[i] = sum[i] / ALL_cls;
 	}
-	k = 0; 
-	for(i = 1; i < 2; i++) {
-		if(sum[k] < sum[i]) {
-	 		k = i;
-	 	}
+	maxAver = average[0];
+	j = 0;
+	for(i = 1; i < MAX_NUM; i++) {
+		if(maxAver < average[i]) {
+			maxAver = average[i];
+			j = i;
+		}
 	}
-	printf("\n平均分最高为第%d名同学：%s %s %.2f\n", k + 1, stu[k].stuNum, stu[k].name, sum[k]);
-	// 功能五
+	printf("\n平均分最高为第%d名同学：%s %s %.2f\n", j + 1, stu[j].stuNum, stu[j].name, maxAver);
+} 
+
+// 读取全部 
+void readAll() {
+	int i;
 	FILE *fp;
-	fopen(&fp, "baocun.txt", "wb");
-	fwrite(stu, sizeof(Student), 2, fp); // 写N条信息全部写入文件
-	fclose(fp); 	
+	printf("\n**************学生信息**************\n\n");
+	printf("学号\t姓名\t成绩一\t成绩二\t成绩三\n"); 
+	if((fp = fopen("stud.dat", "rb")) == NULL) {
+		printf("cannot open file\n");
+		exit(0);
+	}
+	for(i = 0; i < MAX_NUM; i++) {
+	    fseek(fp, i * sizeof(struct Student), 0);
+		fread(&stu[i], sizeof(struct Student), 1, fp);
+		printf("%s\t%s\t%.2f\t%.2f\t%.2f\n", stu[i].stuNum, stu[i].name, stu[i].score[0], stu[i].score[1], stu[i].score[2]);		
+	}
+	printf("\n**************学生信息**************\n");
+	fclose(fp);
+}
+
+// 交换函数
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+} 
+
+int main() {	
+	input();
+	single();
+	aver();
+	readAll();
 }
